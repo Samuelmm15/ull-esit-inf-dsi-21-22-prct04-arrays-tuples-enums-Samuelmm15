f0ecd537bbe9c6da79db59f10cb255b733757820
Samuel Martín <alu0101359526@ull.edu.es>
2022-03-06 12:13:40 +0000

    Advanced

---
+++ P4/src/ejercicio-8.ts
@@ -0,0 +1,71 @@
+type point = {
+  x: number;
+  y: number;
+}
+
+function pointsAdd(firstpoint: point, secondPoint: point): point {
+  const resultPoint: point = {
+    x: 0,
+    y: 0,
+  };
+  resultPoint.x = firstpoint.x + secondPoint.x;
+  resultPoint.y = firstpoint.x + secondPoint.y;
+  return resultPoint;
+}
+
+function pointsSubtract(firstpoint: point, secondPoint: point): point {
+  const resultPoint: point = {
+    x: 0,
+    y: 0,
+  };
+  resultPoint.x = firstpoint.x - secondPoint.x;
+  resultPoint.y = firstpoint.x - secondPoint.y;
+  return resultPoint;
+}
+
+// eslint-disable-next-line max-len
+function productOperation(pointToComprobe: point, numberToOperate: number): point {
+  const resultPoint: point = {
+    x: 0,
+    y: 0,
+  };
+  resultPoint.x = pointToComprobe.x * numberToOperate;
+  resultPoint.y = pointToComprobe.x * numberToOperate;
+  return resultPoint;
+}
+
+function euclideanDistance(firstpoint: point, secondPoint: point): number {
+  let result: number = 0;
+  // eslint-disable-next-line max-len
+  const resultX: number = (firstpoint.x - secondPoint.x) * (firstpoint.x - secondPoint.x);
+  // eslint-disable-next-line max-len
+  const resultY: number = (firstpoint.y - secondPoint.y) * (firstpoint.y - secondPoint.y);
+  result = Math.sqrt(resultX + resultY);
+  return result;
+}
+
+const firstPointToIntroduce: point = {
+  x: 0,
+  y: 0,
+};
+const secondPointToIntroduce: point = {
+  x: 1,
+  y: 1,
+};
+// eslint-disable-next-line max-len
+let resultPointsOperations: point = pointsAdd(firstPointToIntroduce, secondPointToIntroduce);
+// eslint-disable-next-line max-len
+console.log(`EL resultado de la suma con puntos los puntos introducidos es (${resultPointsOperations.x}, ${resultPointsOperations.y})`);
+// eslint-disable-next-line max-len
+resultPointsOperations = pointsSubtract(firstPointToIntroduce, secondPointToIntroduce);
+// eslint-disable-next-line max-len
+console.log(`EL resultado de la resta con puntos los puntos introducidos es (${resultPointsOperations.x}, ${resultPointsOperations.y})`);
+// eslint-disable-next-line max-len
+resultPointsOperations = productOperation(secondPointToIntroduce, 3);
+// eslint-disable-next-line max-len
+console.log(`EL resultado de la resta con puntos los puntos introducidos es (${resultPointsOperations.x}, ${resultPointsOperations.y})`);
+// eslint-disable-next-line max-len
+const resultEuclideanDistance = euclideanDistance(firstPointToIntroduce, secondPointToIntroduce);
+// eslint-disable-next-line max-len
+console.log(`La distancia euclídea para los puntos introducidos es: ${resultEuclideanDistance}`);
+
