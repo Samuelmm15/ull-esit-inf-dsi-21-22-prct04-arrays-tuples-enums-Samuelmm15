47f192d7f778cf45a7eacda1a05ad5b1d6ab972f
Samuel Martín <alu0101359526@ull.edu.es>
2022-03-06 11:00:22 +0000

    Advanced

---
+++ P4/src/ejercicio-7.ts
@@ -0,0 +1,18 @@
+// PREGUNTAR EN CLASE CÓMO SE DEBERÍA DE HACER
+function multiplyAll(arrayToMultiply: number[]): (a: number) => number[] {
+  return (num = 3) => {
+    const resultArray: number[] = [];
+    let result: number = 0;
+    let i: number = 0;
+    while (i != length) {
+      result = arrayToMultiply[i] * num;
+      resultArray.push(result);
+      i++;
+    }
+    return resultArray;
+  };
+}
+
+const arrayExample: number[] = [2, 6, 8];
+const resultMultiplyAll = multiplyAll(arrayExample);
+console.log(`El array obtenido tras la multiplicación es ${resultMultiplyAll}`);
